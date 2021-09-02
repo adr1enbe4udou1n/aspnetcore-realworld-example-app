b5162058e1d39dbb9fd228726adb41fba8c3e086
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-09-02 19:56:13 +0200

    finish seeds

--- tools/Application.Tools/Seeders/ArticlesSeeder.cs
+++ tools/Application.Tools/Seeders/ArticlesSeeder.cs
@@ -29,6 +29,10 @@ namespace Application.Tools.Seeders
                 .RuleFor(a => a.Body, f => f.Lorem.Paragraphs(5))
                 .RuleFor(a => a.AuthorId, f => f.PickRandom(users).Id)
                 .RuleFor(a => a.CreatedAt, f => f.Date.Recent(90))
+                .RuleFor(a => a.FavoredUsers, f => f.PickRandom(users, f.Random.Number(5))
+                    .Select(u => new ArticleFavorite { UserId = u.Id })
+                    .ToList()
+                )
                 .Generate(500);
 
             await _context.Articles.AddRangeAsync(articles, cancellationToken);
@@ -51,7 +55,7 @@ namespace Application.Tools.Seeders
                 new Faker<Tag>()
                     .RuleFor(a => a.Name, f => f.Lorem.Word())
                     .RuleFor(a => a.Articles, f => f.PickRandom(articles, f.Random.Number(10))
-                        .Select(i => new ArticleTag { ArticleId = i.Id })
+                        .Select(a => new ArticleTag { ArticleId = a.Id })
                         .ToList()
                     )
                     .Generate(100),
--- tools/Application.Tools/Seeders/UsersSeeder.cs
+++ tools/Application.Tools/Seeders/UsersSeeder.cs
@@ -1,9 +1,12 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Interfaces;
 using Application.Tools.Interfaces;
 using Bogus;
 using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Tools.Seeders
 {
@@ -27,15 +30,22 @@ namespace Application.Tools.Seeders
                 Password = _passwordHasher.Hash("password"),
             });
 
-            await _context.Users.AddRangeAsync(
-                new Faker<User>()
-                    .RuleFor(m => m.Name, f => f.Person.FullName)
-                    .RuleFor(m => m.Email, f => f.Person.Email)
-                    .RuleFor(m => m.Password, _passwordHasher.Hash("password"))
-                    .Generate(10),
-                cancellationToken
-            );
+            var users = new Faker<User>()
+                .RuleFor(m => m.Name, f => f.Person.FullName)
+                .RuleFor(m => m.Email, f => f.Person.Email)
+                .RuleFor(m => m.Password, _passwordHasher.Hash("password"))
+                .Generate(50);
 
+            await _context.Users.AddRangeAsync(users, cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            users.ForEach(u =>
+            {
+                u.Followers = new Faker().PickRandom(users, new Faker().Random.Number(5))
+                    .Select(u => new FollowerUser { FollowerId = u.Id })
+                    .ToList();
+            });
             await _context.SaveChangesAsync(cancellationToken);
         }
     }
