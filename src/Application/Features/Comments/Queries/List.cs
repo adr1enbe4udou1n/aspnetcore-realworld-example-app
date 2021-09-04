f09bddd567b38be0b28366b374bf89a92daae09e
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-09-04 13:03:10 +0200

    dev comments list

--- src/Application/Features/Comments/Queries/List.cs
+++ src/Application/Features/Comments/Queries/List.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Extensions;
 using Application.Features.Articles.Queries;
 using Application.Interfaces;
 using Application.Support;
+using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Comments.Queries
 {
@@ -22,25 +26,27 @@ namespace Application.Features.Comments.Queries
         public AuthorDTO Author { get; set; }
     }
 
-    public class CommentsEnvelope
-    {
-        public IEnumerable<CommentDTO> Comments { get; set; }
-    }
+    public record CommentsEnvelope(IEnumerable<CommentDTO> Comments);
 
     public record CommentsListQuery(string Slug) : IRequest<CommentsEnvelope>;
 
     public class CommentsListHandler : IRequestHandler<CommentsListQuery, CommentsEnvelope>
     {
         private readonly IAppDbContext _context;
+        private readonly IMapper _mapper;
 
-        public CommentsListHandler(IAppDbContext context)
+        public CommentsListHandler(IAppDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        public Task<CommentsEnvelope> Handle(CommentsListQuery request, CancellationToken cancellationToken)
+        public async Task<CommentsEnvelope> Handle(CommentsListQuery request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var article = await _context.Articles.FindAsync(x => x.Slug == request.Slug, cancellationToken);
+            var comments = article.Comments.OrderByDescending(x => x.Id).ToList();
+
+            return new CommentsEnvelope(_mapper.Map<IEnumerable<CommentDTO>>(comments));
         }
     }
 }
--- tests/Application.IntegrationTests/Comments/CommentsListTests.cs
+++ tests/Application.IntegrationTests/Comments/CommentsListTests.cs
@@ -1,7 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Application.Exceptions;
+using Application.Features.Articles.Commands;
+using Application.Features.Articles.Queries;
+using Application.Features.Comments.Commands;
 using Application.Features.Comments.Queries;
+using Domain.Entities;
+using FluentAssertions;
 using Xunit;
 
 namespace Application.IntegrationTests.Comments
@@ -11,13 +18,79 @@ namespace Application.IntegrationTests.Comments
         public CommentsListTests(Startup factory) : base(factory) { }
 
         [Fact]
+        public async Task CanListAllCommentsOfNotExistingArticle()
+        {
+            await _mediator.Invoking(m => m.Send(new CommentsListQuery("test-title")))
+                .Should().ThrowAsync<NotFoundException>();
+        }
+
+        [Fact]
         public async Task CanListAllCommentsOfArticle()
         {
-            // TODO
+            await ActingAs(new User
+            {
+                Name = "John Doe",
+                Email = "john.doe@example.com",
+                Bio = "My Bio",
+                Image = "https://i.pravatar.cc/300"
+            });
 
-            var response = await _mediator.Send(new CommentsListQuery("slug-article"));
+            await _mediator.Send(new ArticleCreateCommand(
+                new ArticleCreateDTO
+                {
+                    Title = "Test Title",
+                    Description = "Test Description",
+                    Body = "Test Body",
+                }
+            ));
 
-            // TODO
+            var comments = new List<string>
+            {
+                "Test Comment 1",
+                "Test Comment 2",
+                "Test Comment 3",
+                "Test Comment 4",
+                "Test Comment 5",
+            };
+
+            foreach (var c in comments)
+            {
+                await _mediator.Send(new CommentCreateCommand("test-title", new CommentCreateDTO
+                {
+                    Body = $"This is John, {c} !",
+                }));
+            }
+
+            await ActingAs(new User
+            {
+                Name = "Jane Doe",
+                Email = "jane.doe@example.com",
+                Bio = "My Bio",
+                Image = "https://i.pravatar.cc/300"
+            });
+
+            foreach (var c in comments)
+            {
+                await _mediator.Send(new CommentCreateCommand("test-title", new CommentCreateDTO
+                {
+                    Body = $"This is Jane, {c} !",
+                }));
+            }
+
+            var response = await _mediator.Send(new CommentsListQuery("test-title"));
+
+            response.Comments.Count().Should().Be(10);
+
+            response.Comments.First().Should().BeEquivalentTo(new CommentDTO
+            {
+                Body = "This is Jane, Test Comment 5 !",
+                Author = new AuthorDTO
+                {
+                    Username = "Jane Doe",
+                    Bio = "My Bio",
+                    Image = "https://i.pravatar.cc/300"
+                },
+            }, options => options.Excluding(x => x.Id).Excluding(x => x.CreatedAt).Excluding(x => x.UpdatedAt));
         }
     }
 }
