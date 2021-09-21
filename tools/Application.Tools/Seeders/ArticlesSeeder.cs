c69cd339fa995948bf5ce08f3396b31b5c52e26e
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2021-09-21 23:40:00 +0200

    refactor seeds

--- tools/Application.Tools/Seeders/ArticlesSeeder.cs
+++ tools/Application.Tools/Seeders/ArticlesSeeder.cs
@@ -28,28 +28,22 @@ namespace Application.Tools.Seeders
                 .RuleFor(a => a.Title, f => f.Lorem.Sentence().TrimEnd('.'))
                 .RuleFor(a => a.Description, f => f.Lorem.Paragraphs(1))
                 .RuleFor(a => a.Body, f => f.Lorem.Paragraphs(5))
-                .RuleFor(a => a.AuthorId, f => f.PickRandom(users).Id)
+                .RuleFor(a => a.Author, f => f.PickRandom(users))
                 .RuleFor(a => a.CreatedAt, f => f.Date.Recent(90))
                 .RuleFor(a => a.FavoredUsers, f => f.PickRandom(users, f.Random.Number(5))
                     .Select(u => new ArticleFavorite { UserId = u.Id })
                     .ToList()
                 )
                 .RuleFor(a => a.Slug, (f, a) => _slugifier.Generate(a.Title))
+                .RuleFor(a => a.Comments, f => new Faker<Comment>()
+                    .RuleFor(a => a.Body, f => f.Lorem.Paragraphs(2))
+                    .RuleFor(a => a.Author, f => f.PickRandom(users))
+                    .RuleFor(a => a.CreatedAt, f => f.Date.Recent(7))
+                    .Generate(f.Random.Number(10))
+                )
                 .Generate(500);
 
             await _context.Articles.AddRangeAsync(articles, cancellationToken);
-
-            await _context.SaveChangesAsync(cancellationToken);
-
-            await _context.Comments.AddRangeAsync(
-                new Faker<Comment>()
-                    .RuleFor(a => a.Body, f => f.Lorem.Paragraphs(2))
-                    .RuleFor(a => a.ArticleId, f => f.PickRandom(articles).Id)
-                    .RuleFor(a => a.AuthorId, f => f.PickRandom(users).Id)
-                    .RuleFor(a => a.CreatedAt, f => f.Date.Recent(7))
-                    .Generate(5000),
-                cancellationToken
-            );
 
             await _context.SaveChangesAsync(cancellationToken);
 
