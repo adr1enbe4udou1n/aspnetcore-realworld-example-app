1f7f63cd6b733b7ed579c90e4419bfea4b3d45c2
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-02-06 15:58:40 +0100

    format

--- src/Infrastructure/Persistence/Migrations/20220206133720_InitialCreate.cs
+++ src/Infrastructure/Persistence/Migrations/20220206133720_InitialCreate.cs
@@ -3,239 +3,238 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Infrastructure.Persistence.Migrations
+namespace Infrastructure.Persistence.Migrations;
+
+public partial class InitialCreate : Migration
 {
-    public partial class InitialCreate : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Tags",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "varchar(255)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Tags", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Tags",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Name = table.Column<string>(type: "varchar(255)", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Tags", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "varchar(255)", nullable: false),
-                    Email = table.Column<string>(type: "varchar(255)", nullable: false),
-                    Password = table.Column<string>(type: "varchar(255)", nullable: true),
-                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Image = table.Column<string>(type: "varchar(255)", nullable: true),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Users",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                Name = table.Column<string>(type: "varchar(255)", nullable: false),
+                Email = table.Column<string>(type: "varchar(255)", nullable: false),
+                Password = table.Column<string>(type: "varchar(255)", nullable: true),
+                Bio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Image = table.Column<string>(type: "varchar(255)", nullable: true),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Users", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Articles",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    AuthorId = table.Column<int>(type: "int", nullable: false),
-                    Title = table.Column<string>(type: "varchar(255)", nullable: false),
-                    Slug = table.Column<string>(type: "varchar(255)", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Articles", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Articles_Users_AuthorId",
-                        column: x => x.AuthorId,
-                        principalTable: "Users",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "Articles",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                AuthorId = table.Column<int>(type: "int", nullable: false),
+                Title = table.Column<string>(type: "varchar(255)", nullable: false),
+                Slug = table.Column<string>(type: "varchar(255)", nullable: false),
+                Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Articles", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_Articles_Users_AuthorId",
+                    column: x => x.AuthorId,
+                    principalTable: "Users",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "FollowerUser",
-                columns: table => new
-                {
-                    FollowingId = table.Column<int>(type: "int", nullable: false),
-                    FollowerId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_FollowerUser", x => new { x.FollowingId, x.FollowerId });
-                    table.ForeignKey(
-                        name: "FK_FollowerUser_Users_FollowerId",
-                        column: x => x.FollowerId,
-                        principalTable: "Users",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                    table.ForeignKey(
-                        name: "FK_FollowerUser_Users_FollowingId",
-                        column: x => x.FollowingId,
-                        principalTable: "Users",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                });
+        migrationBuilder.CreateTable(
+            name: "FollowerUser",
+            columns: table => new
+            {
+                FollowingId = table.Column<int>(type: "int", nullable: false),
+                FollowerId = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_FollowerUser", x => new { x.FollowingId, x.FollowerId });
+                table.ForeignKey(
+                    name: "FK_FollowerUser_Users_FollowerId",
+                    column: x => x.FollowerId,
+                    principalTable: "Users",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Restrict);
+                table.ForeignKey(
+                    name: "FK_FollowerUser_Users_FollowingId",
+                    column: x => x.FollowingId,
+                    principalTable: "Users",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Restrict);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "ArticleFavorite",
-                columns: table => new
-                {
-                    ArticleId = table.Column<int>(type: "int", nullable: false),
-                    UserId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ArticleFavorite", x => new { x.ArticleId, x.UserId });
-                    table.ForeignKey(
-                        name: "FK_ArticleFavorite_Articles_ArticleId",
-                        column: x => x.ArticleId,
-                        principalTable: "Articles",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_ArticleFavorite_Users_UserId",
-                        column: x => x.UserId,
-                        principalTable: "Users",
-                        principalColumn: "Id");
-                });
+        migrationBuilder.CreateTable(
+            name: "ArticleFavorite",
+            columns: table => new
+            {
+                ArticleId = table.Column<int>(type: "int", nullable: false),
+                UserId = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_ArticleFavorite", x => new { x.ArticleId, x.UserId });
+                table.ForeignKey(
+                    name: "FK_ArticleFavorite_Articles_ArticleId",
+                    column: x => x.ArticleId,
+                    principalTable: "Articles",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_ArticleFavorite_Users_UserId",
+                    column: x => x.UserId,
+                    principalTable: "Users",
+                    principalColumn: "Id");
+            });
 
-            migrationBuilder.CreateTable(
-                name: "ArticleTag",
-                columns: table => new
-                {
-                    ArticleId = table.Column<int>(type: "int", nullable: false),
-                    TagId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ArticleTag", x => new { x.ArticleId, x.TagId });
-                    table.ForeignKey(
-                        name: "FK_ArticleTag_Articles_ArticleId",
-                        column: x => x.ArticleId,
-                        principalTable: "Articles",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_ArticleTag_Tags_TagId",
-                        column: x => x.TagId,
-                        principalTable: "Tags",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "ArticleTag",
+            columns: table => new
+            {
+                ArticleId = table.Column<int>(type: "int", nullable: false),
+                TagId = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_ArticleTag", x => new { x.ArticleId, x.TagId });
+                table.ForeignKey(
+                    name: "FK_ArticleTag_Articles_ArticleId",
+                    column: x => x.ArticleId,
+                    principalTable: "Articles",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_ArticleTag_Tags_TagId",
+                    column: x => x.TagId,
+                    principalTable: "Tags",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Comments",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    ArticleId = table.Column<int>(type: "int", nullable: false),
-                    AuthorId = table.Column<int>(type: "int", nullable: false),
-                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Comments", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Comments_Articles_ArticleId",
-                        column: x => x.ArticleId,
-                        principalTable: "Articles",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Comments_Users_AuthorId",
-                        column: x => x.AuthorId,
-                        principalTable: "Users",
-                        principalColumn: "Id");
-                });
+        migrationBuilder.CreateTable(
+            name: "Comments",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                ArticleId = table.Column<int>(type: "int", nullable: false),
+                AuthorId = table.Column<int>(type: "int", nullable: false),
+                Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Comments", x => x.Id);
+                table.ForeignKey(
+                    name: "FK_Comments_Articles_ArticleId",
+                    column: x => x.ArticleId,
+                    principalTable: "Articles",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Comments_Users_AuthorId",
+                    column: x => x.AuthorId,
+                    principalTable: "Users",
+                    principalColumn: "Id");
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_ArticleFavorite_UserId",
-                table: "ArticleFavorite",
-                column: "UserId");
+        migrationBuilder.CreateIndex(
+            name: "IX_ArticleFavorite_UserId",
+            table: "ArticleFavorite",
+            column: "UserId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Articles_AuthorId",
-                table: "Articles",
-                column: "AuthorId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Articles_AuthorId",
+            table: "Articles",
+            column: "AuthorId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Articles_Slug",
-                table: "Articles",
-                column: "Slug",
-                unique: true);
+        migrationBuilder.CreateIndex(
+            name: "IX_Articles_Slug",
+            table: "Articles",
+            column: "Slug",
+            unique: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_ArticleTag_TagId",
-                table: "ArticleTag",
-                column: "TagId");
+        migrationBuilder.CreateIndex(
+            name: "IX_ArticleTag_TagId",
+            table: "ArticleTag",
+            column: "TagId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Comments_ArticleId",
-                table: "Comments",
-                column: "ArticleId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Comments_ArticleId",
+            table: "Comments",
+            column: "ArticleId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Comments_AuthorId",
-                table: "Comments",
-                column: "AuthorId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Comments_AuthorId",
+            table: "Comments",
+            column: "AuthorId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_FollowerUser_FollowerId",
-                table: "FollowerUser",
-                column: "FollowerId");
+        migrationBuilder.CreateIndex(
+            name: "IX_FollowerUser_FollowerId",
+            table: "FollowerUser",
+            column: "FollowerId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Tags_Name",
-                table: "Tags",
-                column: "Name",
-                unique: true);
+        migrationBuilder.CreateIndex(
+            name: "IX_Tags_Name",
+            table: "Tags",
+            column: "Name",
+            unique: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Users_Email",
-                table: "Users",
-                column: "Email",
-                unique: true);
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_Users_Email",
+            table: "Users",
+            column: "Email",
+            unique: true);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "ArticleFavorite");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "ArticleFavorite");
 
-            migrationBuilder.DropTable(
-                name: "ArticleTag");
+        migrationBuilder.DropTable(
+            name: "ArticleTag");
 
-            migrationBuilder.DropTable(
-                name: "Comments");
+        migrationBuilder.DropTable(
+            name: "Comments");
 
-            migrationBuilder.DropTable(
-                name: "FollowerUser");
+        migrationBuilder.DropTable(
+            name: "FollowerUser");
 
-            migrationBuilder.DropTable(
-                name: "Tags");
+        migrationBuilder.DropTable(
+            name: "Tags");
 
-            migrationBuilder.DropTable(
-                name: "Articles");
+        migrationBuilder.DropTable(
+            name: "Articles");
 
-            migrationBuilder.DropTable(
-                name: "Users");
-        }
+        migrationBuilder.DropTable(
+            name: "Users");
     }
 }
