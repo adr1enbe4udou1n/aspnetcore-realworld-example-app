8bbb8867467a0f7c1e7307b700f4f41afe77a341
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-06-04 21:21:54 +0200

    format

--- tools/Conduit.Tools/Program.cs
+++ tools/Conduit.Tools/Program.cs
@@ -2,11 +2,12 @@ using Conduit.Infrastructure;
 using Conduit.Tools.Interfaces;
 using Conduit.Tools.Seeders;
 
+using ConsoleAppFramework;
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using Tools.Commands;
-using ConsoleAppFramework;
-using Microsoft.Extensions.Configuration;
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
