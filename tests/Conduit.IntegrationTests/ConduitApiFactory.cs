d5cc5e57e2614b83a3288c5ab8c662aa3f35fbfb
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-09-01 02:20:16 +0200

    add testcontainer

--- tests/Conduit.IntegrationTests/ConduitApiFactory.cs
+++ tests/Conduit.IntegrationTests/ConduitApiFactory.cs
@@ -22,6 +22,7 @@ public class ConduitApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
         .WithUsername("main")
         .WithPassword("main")
         .WithImage("postgres:16")
+        .WithBindMount("/var/run/docker.sock", "/var/run/user/1000/docker.sock")
         .Build();
 
     public async Task MigrateDatabase()
