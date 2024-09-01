0d2153297673c561c217b9003c21596347e6306a
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-09-01 23:15:36 +0200

    give up

--- tests/Conduit.IntegrationTests/ConduitApiFixture.cs
+++ tests/Conduit.IntegrationTests/ConduitApiFixture.cs
@@ -1,3 +1,5 @@
+
+using Conduit.Application.Interfaces;
 using Conduit.Infrastructure.Persistence;
 using Conduit.IntegrationTests.Events;
 
@@ -7,25 +9,66 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using Testcontainers.PostgreSql;
+
+using Xunit;
+
 namespace Conduit.IntegrationTests;
 
-public class ConduitApiFixture : WebApplicationFactory<Program>
+public class ConduitApiFixture : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    public ConduitApiFixture()
+    private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder()
+        .WithDatabase("main")
+        .WithUsername("main")
+        .WithPassword("main")
+        .WithImage("postgres:16")
+        .WithNetworkAliases("postgres-test")
+        .Build();
+
+    public async Task MigrateDatabase()
     {
         using var scope = Services.CreateScope();
 
         using var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-        dbContext.Database.Migrate();
+        await dbContext.Database.MigrateAsync();
     }
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder
             .UseEnvironment("Testing")
-            .ConfigureServices(services => services.AddLogging((builder) =>
-                builder.AddProvider(new SqlCounterLoggerProvider()))
-            );
+            .ConfigureServices(services =>
+            {
+                services.AddLogging((builder) =>
+                    builder.AddProvider(new SqlCounterLoggerProvider()));
+
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<IAppDbContext, AppDbContext>((options) =>
+                {
+                    options
+                        .UseLazyLoadingProxies()
+                        .UseNpgsql(_postgreSqlContainer.GetConnectionString());
+                });
+            });
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+        await MigrateDatabase();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _postgreSqlContainer.DisposeAsync();
+        await base.DisposeAsync();
     }
 }
