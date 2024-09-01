56043f70a4cfd8dcedbba402ff3eecb983d5057d
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-09-01 23:23:00 +0200

    give up

--- tests/Conduit.IntegrationTests/ConduitApiFixture.cs
+++ tests/Conduit.IntegrationTests/ConduitApiFixture.cs
@@ -64,6 +64,7 @@ public class ConduitApiFixture : WebApplicationFactory<Program>, IAsyncLifetime
     public async Task InitializeAsync()
     {
         await _postgreSqlContainer.StartAsync();
+        await Task.Delay(600000);
         await MigrateDatabase();
     }
 
