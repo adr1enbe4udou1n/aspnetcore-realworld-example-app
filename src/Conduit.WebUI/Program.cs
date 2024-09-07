9fbb53835ff6b5358bf0fd548a7b1c95b53cae3f
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-09-07 21:58:39 +0200

    Revert "remove telemetry"
    This reverts commit 80846c8cfca8a3beb8087954bf13d975abb714c2.

--- src/Conduit.WebUI/Program.cs
+++ src/Conduit.WebUI/Program.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 using Conduit.Application;
 using Conduit.Infrastructure;
 using Conduit.Infrastructure.Persistence;
@@ -7,7 +9,18 @@ using Conduit.WebUI.OptionsSetup;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
+using Npgsql;
+
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 // Add services to the container.
 builder.Services
@@ -18,11 +31,64 @@ builder.Services
 builder.Services
     .ConfigureOptions<JwtOptionsSetup>()
     .ConfigureOptions<JwtBearerOptionsSetup>()
-    .AddAuthorization()
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer();
 
+if (!builder.Environment.IsEnvironment("Testing"))
+{
+    builder.Services
+        .AddHealthChecks()
+        .AddDbContextCheck<AppDbContext>();
+
+    builder.Services
+        .AddOpenTelemetry()
+        .WithMetrics(builder =>
+        {
+            builder
+                .AddAspNetCoreInstrumentation()
+                .AddPrometheusExporter()
+                .AddMeter(
+                    "Microsoft.AspNetCore.Hosting",
+                    "Microsoft.AspNetCore.Server.Kestrel"
+                );
+        })
+        .WithTracing(b =>
+        {
+            b
+                .SetResourceBuilder(ResourceBuilder
+                    .CreateDefault()
+                    .AddService("AspNetCore.Conduit")
+                    .AddTelemetrySdk()
+                )
+                .AddAspNetCoreInstrumentation(b =>
+                {
+                    b.Filter = ctx =>
+                    {
+                        return ctx.Request.Path.StartsWithSegments(
+                            "/api", StringComparison.OrdinalIgnoreCase
+                        );
+                    };
+                })
+                .AddEntityFrameworkCoreInstrumentation()
+                .AddNpgsql()
+                .AddOtlpExporter();
+        });
+
+    builder.Host.UseSerilog((context, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture));
+}
+
 var app = builder.Build();
+
+app.MapReverseProxy();
+
+if (!app.Environment.IsEnvironment("Testing"))
+{
+    app.UseSerilogRequestLogging();
+    app.MapHealthChecks("/healthz");
+    app.MapPrometheusScrapingEndpoint();
+}
 
 app.UseSwagger(c =>
 {
