13ffe5893ed38f92dc756541a7f770686b5f0e75
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-11-25 20:32:50 +0100

    up pg test

--- tests/Conduit.IntegrationTests/ConduitApiFixture.cs
+++ tests/Conduit.IntegrationTests/ConduitApiFixture.cs
@@ -21,7 +21,7 @@ public class ConduitApiFixture : WebApplicationFactory<Program>, IAsyncLifetime
         .WithDatabase("main")
         .WithUsername("main")
         .WithPassword("main")
-        .WithImage("postgres:17")
+        .WithImage("postgres:17.1")
         .Build();
 
     public async Task MigrateDatabase()
